cb4dccdeb53cecca54f3ca2e5538dc1b80036075
Ivan Kruchkou <89302275+BreakVan@users.noreply.github.com>
2023-05-10 18:47:53 +0300

    fixes

--- JS/Footer.js
+++ JS/Footer.js
@@ -2,10 +2,12 @@ window.onload = function() {
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", "../XML/footer.xml", true);
     xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("myFooter228").innerHTML = this.responseText;
-      } else {
-        console.error("Failed to load footer: status " + this.status);
+      if (this.readyState == 4) {
+        if (this.status == 200) {
+          document.getElementById("myFooter228").innerHTML = this.responseText;
+        } else {
+          console.error("Failed to load footer: status " + this.status);
+        }
       }
     };
     xhttp.send();
